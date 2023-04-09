ebb366e4925d2633ccfac87c7e0865ac2fcccc2d
Carolina <boitelc@ufl.edu>
2023-04-09 11:47:37 -0400

    Got user input

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
-//
-// Created by elisa on 4/9/2023.
-//
 #include <iostream>
+#include <string>
 
-int main(){
 
+using namespace std;
+
+
+int main() {
+    cout << "Welcome to the Movie Matchmaker!" << endl;
+    cout << "--------------------------------" << endl;
+    cout << "Here We Will Return a Sorted Selection of Movie Recommendations!" << endl;
+    cout << "Please Choose a Sorting Category: Year or Genre" << endl;
+    string typeInput;
+    string sortInput;
+    cin >> typeInput;
+    if (typeInput == "Year" || typeInput == "year") {
+        cout << "Please Choose a Sorting Algorithm: Shell Sort or Quick Sort";
+        cin >> sortInput;
+        if (sortInput == "Shell Sort" || sortInput == "shell sort") {
+            cout << "Begin Shell Sort by Year...." << endl;
+        } else if (typeInput == "Genre" || typeInput == "genre") {
+            cout << "Please Choose a Sorting Algorithm: Shell Sort or Quick Sort";
+            cin >> sortInput;
+            if (sortInput == "Shell Sort" || sortInput == "shell sort") {
+                cout << "Begin Shell Sort by Genre...." << endl;
+            }
+            return 0;
+        }
+    }
 }
