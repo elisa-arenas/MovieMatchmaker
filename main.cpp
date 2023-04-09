53e18867222d7c1b7978438242ddac9257ec80cd
Carolina <boitelc@ufl.edu>
2023-04-09 12:14:37 -0400

    Fixed user input

--- main.cpp
+++ main.cpp
@@ -1,30 +1,40 @@
 #include <iostream>
 #include <string>
-
+#include "Movie.h"
 
 using namespace std;
-
 
 int main() {
     cout << "Welcome to the Movie Matchmaker!" << endl;
     cout << "--------------------------------" << endl;
     cout << "Here We Will Return a Sorted Selection of Movie Recommendations!" << endl;
     cout << "Please Choose a Sorting Category: Year or Genre" << endl;
+
     string typeInput;
     string sortInput;
     cin >> typeInput;
+
+    // YEAR
     if (typeInput == "Year" || typeInput == "year") {
-        cout << "Please Choose a Sorting Algorithm: Shell Sort or Quick Sort";
+        cout << "Please Choose a Sorting Algorithm: Shell Sort or Quick Sort" << endl;
         cin >> sortInput;
-        if (sortInput == "Shell Sort" || sortInput == "shell sort") {
-            cout << "Begin Shell Sort by Year...." << endl;
-        } else if (typeInput == "Genre" || typeInput == "genre") {
-            cout << "Please Choose a Sorting Algorithm: Shell Sort or Quick Sort";
-            cin >> sortInput;
-            if (sortInput == "Shell Sort" || sortInput == "shell sort") {
-                cout << "Begin Shell Sort by Genre...." << endl;
-            }
-            return 0;
+        if (sortInput == "Shell" || sortInput == "shell") {
+            cout << "Beginning Shell Sort by Year...." << endl;
+        }
+        else if(sortInput == "Quick" || sortInput == "quick") {
+            cout << "Beginning Quick Sort by Year...." << endl;
         }
     }
+    // GENRE
+    else if (typeInput == "Genre" || typeInput == "genre") {
+        cout << "Please Choose a Sorting Algorithm: Shell Sort or Quick Sort" << endl;
+        cin >> sortInput;
+        if (sortInput == "Shell" || sortInput == "shell") {
+            cout << "Beginning Shell Sort by Genre...." << endl;
+        }
+        else if(sortInput == "Quick" || sortInput == "quick") {
+            cout << "Beginning Quick Sort by Genre...." << endl;
+        }
+    }
+    return 0;
 }
